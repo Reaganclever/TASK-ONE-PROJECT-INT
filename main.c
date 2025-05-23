4d5a227b6cb64cc02aa9b2a393f9e06940f5e5db
reaganclever <kasulereagan86@gmail.com>
2025-05-23 22:43:37 +0300

    TOPI-2 COMMIT#1 ON BRANCH#1

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int age = 20;
+    float height = 5.9f;
+    double distance = 12345.6789;
+    char initial = 'A';
+
+    printf(" INTEGER VARIABLE NAMED AGE IS %d\n\n" , age);
+    printf("FLOAT VARIABLE NAMED HEIGHT IS %f\n\n" , height);
+    printf("DOUBLE VARIABLE NAMED DISTANCE IS %lf\n\n" , distance);
+    printf("CHARACTER VARIABLE NAMED INITIAL IS %c\n\n" , initial);
+
+    return 0;
+}
